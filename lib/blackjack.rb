f152b7d49c02ee07dccb576e88a2c013f114153a
Katie Hoffman <ktahoffman@gmail.com>
2014-09-17 14:11:04 -0400

    remove passing code

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,69 +1,44 @@
-##################
-# shared methods #
-##################
-
 def deal_card
-  rand(1..11)
+  # code #deal_card here
 end
 
-def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
+def display_card_total
+  # code #display_card_total here
 end
-
-##########
-# rounds #
-##########
 
 def welcome
-  puts "Welcome to the Blackjack Table"
+  # code #welcome here
 end
 
 def prompt_user
-  puts "Type 'h' to hit or 's' to stay"
+  # code #prompt_user here
 end
 
 def initial_round
-  first_round = deal_card + deal_card
-  display_card_total(first_round)
-  return first_round
+  # code #initial_round here
 end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+def end_game
+  # code #end_game here
 end
 
 def get_user_input
-  gets.chomp.strip
+  # code #get_user_input here
 end
 
-def hit?(card_total)
-  prompt_user
-  user_input = get_user_input
-  if user_input == "h"
-    card_total += deal_card
-  elsif user_input == "s"
-    card_total
-  else
-    invalid_command
-  end
+def hit?
+  # code hit? here
 end
 
 def invalid_command
-  puts "Sorry, not a valid command"
-  get_user_input
+  # code invalid_command here
 end
 
-##########
-# runner #
-##########
+#####################################################
+# get every test to pass before coding runner below #
+#####################################################
 
 def runner
-  welcome
-  card_total = initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
+  # code runner here
 end
     
